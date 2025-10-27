bb02209480fb1e380d90e2054816531f2dadaabb
kwakkh <kwakkh@ironmace.com>
2025-10-27 09:50:21 +0900

    Main 보충 2025.10.27

--- Launch.h
+++ Launch.h
@@ -1,3 +1,8 @@
+/**
+ * kwakkh
+ * - LaunchWindow.cpp
+ */
+
 #include "LaunchEngineLoop.h"
 
 /** FPlatformTime::Seconds()가 처음 초기화된 시간 (before main)
@@ -5,11 +10,14 @@
 */
 double GStartTime = FPlatformTime::InitTiming();
 
-// 엔진의 루프를 관장하고 있는 싱글톤 객체
-FEngineLoop	GEngineLoop;
+/** kwakkh
+ * 언리얼 엔진에서 메인 루프를 진행하고 관장하고 있는 전역 변수
+ */
+ FEngineLoop	GEngineLoop;
 
 /** preinit the engine loop */
 // 5 - Foundation - Entry - EnginePreInit
+// Kwakkh : 언리얼 엔진에서 메인 루프의 초기화를 시작하는 함수
 int32 EnginePreInit( const TCHAR* CmdLine )
 {
 	int32 ErrorLevel = GEngineLoop.PreInit( CmdLine );
@@ -26,6 +34,7 @@ int32 EditorInit(IEngineLoop& EngineLoop)
  * Ticks the engine loop 
  */
 // 8 - Foundation - Entry - EngineTick
+// kwakkh : GEngineLoop 를 통해 엔진의 메인 루프를 실행하는 함수
 LAUNCH_API void EngineTick( void )
 {
 	GEngineLoop.Tick();
@@ -47,6 +56,7 @@ LAUNCH_API void EngineExit( void )
  * 이렇게 별도의 함수로 분리한 이유는 디버거가 연결되어 있는지 여부에 따라, 디버그 빌드와 릴리즈 빌드에서 각각 다른 방식으로 오류를 처리할 수 있게 하기 위함.
  */
 // 4 - Foundation - Entry - GuardMain
+// kwakkh : 언리얼 엔진의 메인 로직 진행 함수
 int32 GuardedMain( const TCHAR* CmdLine )
 {
 	//....
@@ -56,7 +66,8 @@ int32 GuardedMain( const TCHAR* CmdLine )
 	// "-waitforattach" 또는 "-WaitForDebugger"가 지정된 경우 시작을 중지하고 디버거가 연결될 때까지 기다렸다가 계속 진행
 
     /** kwakkh 
-    * 명령어 인수를 "-waitforattach"로 전달하는 것은 엔진을 시작할 때 디버깅할 때 유용
+    * 명령어 인자에 "-waitforattach"가 있을 경우 엔진 시작시 디버거가 붙을 때까지 루프를 돌며 대기한다.
+	* - 엔진 시작지점부터 디버깅을 진행할 때 유용하게 사용 가능
     */
 	const bool bWaitForDebuggerAndBreak = FParse::Param(CmdLine, TEXT("waitforattach")) || FParse::Param(CmdLine, TEXT("WaitForDebugger")); 
 	const bool bWaitForDebugger = bWaitForDebuggerAndBreak || FParse::Param(CmdLine, TEXT("WaitForAttachNoBreak")) || FParse::Param(CmdLine, TEXT("WaitForDebuggerNoBreak"));
@@ -71,8 +82,12 @@ int32 GuardedMain( const TCHAR* CmdLine )
 
 		if (bWaitForDebuggerAndBreak)
 		{
-            // kwakkh : 여기서 멈춤
-            // - 시작 지점을 디버깅하는 데 매우 유용
+			/**kwakkh
+			 * - 여기서 멈춤
+			 * - 코드를 이용해 DebugBreakPoint를 설정하는 함수
+			 * - 언리얼에서 코드 레벨에서 디버깅을 진행하기 위해 존재하는 함수
+			 * - 시작 지점을 디버깅하는 데 매우 유용
+			 */
 			UE_DEBUG_BREAK();
 		}
 	}
@@ -84,7 +99,9 @@ int32 GuardedMain( const TCHAR* CmdLine )
 	// Super early init code. DO NOT MOVE THIS ANYWHERE ELSE!
     /**
      * kwakkh
+	 * - 언리얼 엔진 코멘트에서도 다른 곳으로 이동시키지 말라고 명시되어 있을 정도로 중요한 함수
      * - CoreDelegates, CoreUObjectDelegates, WorldDelegates, ... 
+	 * - 위의 델리게이트를 통해 엔진 시작 지점에서 초기화를 진행해야하는 중요 로직을 처리리
      * - 이 Delegate Class들은 기억하기 좋음
      * - 언리얼 엔진은 위와 같이 위임자 클래스를 증명하는 형태로 코드를 주입하는 방법을 제공
      * - 여기서 언리얼 엔진의 시작 지점에 코드를 삽입할 수 있다
@@ -92,7 +109,11 @@ int32 GuardedMain( const TCHAR* CmdLine )
 	FCoreDelegates::GetPreMainInitDelegate().Broadcast();
 
 	// make sure GEngineLoop::Exit() is always called.
-    // kwakkh : 이 패턴은 언리얼 엔진에서 자주 사용
+	/** kwakkh
+	 * - 구조체의 정의(EngineLoolCleanupGuard)와 변수(CleanupGuard) 선언을 동시해 진행.
+	 * - GuardMain 함수가 끝나게 될 경우 CleanupGuard 변수도 사라지게 되고 그 즉시 EngineExit() 함수를 호출
+	 * - 이 패턴은 언리얼 엔진에서 자주 사용
+	 */
 	struct EngineLoopCleanupGuard 
 	{ 
 		~EngineLoopCleanupGuard()
@@ -154,7 +175,8 @@ int32 GuardedMain( const TCHAR* CmdLine )
 	if (!GUELibraryOverrideSettings.bIsEmbedded)
 	{
         // kwakkh : IsEngineExitRequested() == GIsRequestingExit
-        // - main loop of the engine
+        // - 언리얼 엔진의 메인 루프
+		// - 종료 요청이 존재하기 전까지 무한루프인 것을 확인할 수 있다.
 		while( !IsEngineExitRequested() )
 		{
 			EngineTick();
@@ -177,6 +199,7 @@ int32 GuardedMain( const TCHAR* CmdLine )
  */
 
 // 3 - Foundation - Entry - GuardedMainWrapper
+// kwakkh : GuardMain, 즉 엔진의 메인을 호출 후 에러 레벨에 대한 체크를 위해 한 번 래핑을 진행한 함수
 LAUNCH_API int32 GuardedMainWrapper( const TCHAR* CmdLine )
 {
 	int32 ErrorLevel = 0;
@@ -210,6 +233,7 @@ AUNCH_API int32 LaunchWindowsStartup( HINSTANCE hInInstance, HINSTANCE hPrevInst
 {
     int32 ErrorLevel = 0;
 
+	// kwakkh : 명령줄을 처리하는 단계
     if (!CmdLine)
     {
         /** kwakkh : 리얼 엔진은 또한 명령 라인을 자신만의 스타일로 변환하도록 처리 */
@@ -226,6 +250,8 @@ AUNCH_API int32 LaunchWindowsStartup( HINSTANCE hInInstance, HINSTANCE hPrevInst
     // 어쨋든 크래쉬 덤프를 줘야 한다.
     // 그것을 위한 아래의 코드들
     /** 구조적 예외 처리 (SEH, Structured Exception Handling)를 사용해 충돌을 처리
+	 * SEH란 운영체제(OS)를 통해 하드웨어와 소프트웨어 예외 모두 처리하는 기본 예외 처리 메커니즘
+	 * 아래의 코드는 GuardedMainWrapper에서 발생하는 모든 예외사오항에 대해서 기록을 남기고 또는 추가적 처리를 진행
      * SEH가 OS(커널모드)에 의해 트리거 된다는 것
      * SEH의 작동 방식은 정적이지 않으며, MS와 같은 OS 제공업체에 의해 언제든지 변경될 수 있다.
      * - 관련 OS 보안 자체이기 때문
@@ -266,6 +292,7 @@ AUNCH_API int32 LaunchWindowsStartup( HINSTANCE hInInstance, HINSTANCE hPrevInst
 }
 
 // 1 - Foundation - Entry - LaunchWindowsShutdown
+// kwakkh : 언리얼 엔진 종료 함수
 LAUNCH_API void LaunchWindowsShutdown()
 {
 	// Final shut down.
@@ -279,7 +306,7 @@ LAUNCH_API void LaunchWindowsShutdown()
 }
 
 // 0 - Foundation - Entry - BEGIN
-/** kwakkh : 언리얼의 시작 위치 */
+/** kwakkh : 언리얼 엔진의 윈도우 플랫폼 기준 시작 함수 */
 int32 WINAPI WinMain(_In_ HINSTANCE hInInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ char* pCmdLine, _In_ int32 nCmdShow)
 {
     int32 Result = LaunchWindowsStartup(hInInstance, hPrevInstance, pCmdLine, nCmdShow, nullptr);
--- LaunchEngineLoop.h
+++ LaunchEngineLoop.h
@@ -5,7 +5,13 @@ ENGINE_API UEngin* GEngine = NULL;
 /**
  * Implements the main engine loop.	
  */
-class FEngineLoop
+
+ /**
+  * kwakkh
+  * 언리얼 엔진에서 메인 루프를 진행하기 위해 별도로 존재하는 클래스.
+  * - 엔진의 메인 Tick을 관리하는 것과 동일 
+  */
+ class FEngineLoop
 #if WITH_ENGINE
 	: public IEngineLoop
 #endif
@@ -15,6 +21,8 @@ public:
      * Pre-Initialize the main loop - parse command line, sets up GIsEditor, etc.
      */
     // 6 - Foundation - Entry - FEngineLoop::PreInit
+    // kwakkh : 메인 루프 초기화의 로직들이 담겨있는 함수
+    // - 명령줄을 분석, GIsEditor 설정 등을 담당
     int32 PreInit(const TCHAR* CmdLine)
     {
         //...
@@ -22,16 +30,33 @@ public:
 
     /** Advances the main loop. */
      // 9 - Foundation - Entry - FEngineLoop::Tick
+     // kwakkh : 엔진의 메인 루프를 담당하는 함수
 	virtual void Tick() override
     {
         // set FApp::CurrentTime, FApp::DeltaTime and potentially wait to enforce max tick rate
 		{
-			QUICK_SCOPE_CYCLE_COUNTER(STAT_FEngineLoop_UpdateTimeAndHandleMaxTickRate);
+            // ...
+
+            /**
+             * kwakkh
+             * CurrentTime과 DeltaTime을 제공하는 함수
+             * - 그 뿐만이 아니라 여러 스레드에 대해서 동기화 진행
+             * ex)
+             * GameThread와 RenderThread가 병렬로 실행되고 있을 때, 
+             * GameThread가 먼저 끝났다고 GameThread가 먼저 실행되면 엔진 전체의 동기화가 꺠져버리기 때문에 
+             * FrameNumber와 같은 변수를 컨트롤해 서로를 동기화 한다.
+             */
 			GEngine->UpdateTimeAndHandleMaxTickRate();
-			GEngine->SetSimulationLatencyMarkerStart(CurrentFrameCounter);
+			
+            // ...
 		}
 
         // main game engine tick (world, game objects, etc.)
+        /**
+         * kwakkh
+         * 게임의 메인 루프
+         * - Lyra를 예로들면, ULyraEditorEngine -> UUnrealEdEngine -> UEditorEngine 순으로 실행
+         */
 		GEngine->Tick(FApp::GetDeltaTime(), bIdleMode);
     }
 }
