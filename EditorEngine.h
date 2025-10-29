0ca7b5cb91734475d7f72154d2a6da3f5ad845c2
kwakkh <kwakkh@ironmace.com>
2025-10-29 09:47:48 +0900

    Engine 보충 2025.10.29

--- EditorEngine.h
+++ EditorEngine.h
@@ -4,7 +4,8 @@
 // 14 - Foundation - Entry - EWorldType
 /**
  * kwakkh
- * - 현재는 EngineTypes.h 에 위치함
+ * - 월드에 대한 다양한 타입을 선언한 열거형
+ * - EngineTypes.h 에 위치함
  */
 namespace EWorldType
 {
@@ -64,7 +65,7 @@ namespace EWorldType
 // 13 - Foundation - Entry - FWorldContext
 /** kwakkh
  * - Engine.h 에 위치함
- * - 엔진 레벨에서 사용되는 월드의 Context (UEngine, UEditorEngine 등)
+ * - 엔진 레벨에서 다양한 UWorld들을 처리하기 위한 Context (UEngine, UEditorEngine 등)
  * - FWorldContext를 엔진과 World간의 종속성을 잃는 descriptor로 생각할 수 있음
  * 
  * GameEngine 용
@@ -76,6 +77,10 @@ namespace EWorldType
  * - PIE를 실행하면 새로운 World Context가 생성되며 동시에 두 가지 World Context가 존재
  * - PIE에서 멀티플레이 게임을 실행하려 할 때, 여러 World Context이 존재할 수 있다.
  * - World Context는 맥락은 엔진을 위한 것이므로 다른 곳에서는 FWorldContext를 유지하지 말라. 
+ * 
+ * - 일반적인 게임 상황이라면 World가 1개 존재하겠지만 에디터의 경우엔 동시에 다양한 맵이 존재할 수 있다.
+ * - ex) 에디터에서 Actor의 블루프린트로 간다면 에디터 세상의 맵 1개 블루프린트의 맵 1개 총 2개가 존재하기 된다.
+ * -     이런 상황에서 World가 PIE 인지 GameWorld 인지 등 다양한 판단을 엔진에서 하기 위해 존재하는 구조체
  */
 USTRUCT()
 struct FWorldContext
@@ -83,6 +88,10 @@ struct FWorldContext
 	GENERATED_USTRUCT_BODY()
 
     /** Set CurrentWorld and update external reference pointers to reflect this*/
+    /** kwakkh
+     * - WorldContext에 World를 설정하는 함수
+     * - 중요점은 이 함수를 통해 WorldContext는 World 자체가 아닌, World를 엔진 레벨에서 컨트롤하기 쉽도록 World를 들고 있는 래핑 구조체라는 것.
+     */
 	ENGINE_API void SetCurrentWorld(UWorld *World)
     {
         UWorld* OldWorld = ThisCurrentWorld;
@@ -108,7 +117,7 @@ struct FWorldContext
 	TObjectPtr<class UGameInstance> OwningGameInstance;
 
 private:
-    // kwakkh : Context가 참조하는 World
+    // kwakkh : WorldContext가 참조하는 World
 	TObjectPtr<UWorld>	ThisCurrentWorld;
 }
 
@@ -124,7 +133,7 @@ class UEngine : public UObject, public FExec
 
     //...
 
-    // kwakkh : WorldContext 생성 함수
+    // kwakkh : WorldType에 맞는 WorldContext 생성하고 반환하는 함수 (이 함수에서 World를 세팅하지 않음!)
     FWorldContext& CreateNewWorldContext(EWorldType::Type WorldType)
     {
         FWorldContext* NewWorldContext = new FWorldContext;
@@ -137,6 +146,12 @@ class UEngine : public UObject, public FExec
 
     /** Initialize the game engine. */
     // 17 - Foundation - Entry - UEngine::Init
+    /**
+     * kwakkh
+     * - 게임 엔진을 초기화하는 함수
+     * - Editor 모드일 경우 WorldContext와 World를 Editor 타입으로 만들어 GWorld에 세팅.
+     * - 이 함수를 통해 에디터는 최소한 하나의 월드를 보장 받을 수 있다. 
+     */
     virtual void Init(IEngineLoop* InEngineLoop)
     {
         if (GIsEditor)
@@ -153,7 +168,7 @@ class UEngine : public UObject, public FExec
     }
 
     /** kwakkh
-     * - TIndirectArray 가 뭐지? (TIndirectArray 와 TArray의 차이를 알아보자)
+     * - TIndirectArray의 경우 TArray와 동일하게 데이터를 저장하는 것은 맞으나 데이터를 추가할 때 해당 데이터를 동적할당을 진행하고 배열에서 제외할 때 메모리에서 제거한다.
      * - WorldList는 WorldContext를 관리
      * - WorldContext를 관리하고 생성, 파괴의 주체는 엔진이다.
      */
@@ -163,6 +178,11 @@ class UEngine : public UObject, public FExec
 }
 
 // 10 - Foundation - Entry - UEditorEngine
+/**
+ * kwakkh
+ * - 언리얼 엔진에서 에디터를 담당하는 클래스
+ * - 에디터는 일반적으로 게임에서는 발생하지 않는 다양한 상황이 존대하기 때문에 별도의 클래스가 존재하는 것을 확인할 수 있다.
+ */
 UCLASS(config=Engine, transient, MinimalAPI)
 class UEditorEngine : public UEngine
 {
@@ -170,10 +190,16 @@ public:
 	GENERATED_BODY()
 
 public:
-    /** 에디터 world에 대한 WorldContext를 반환. 현재로서는 에디터에 항상 정확히 하나만 있을 것이다. */
     // 16 - Foundation - Entry - GetEditorWorldContext
+    /** kwakkh :에디터 world에 대한 WorldContext를 반환. 현재로서는 에디터에 항상 정확히 하나만 있을 것이다. */
     FWorldContext& GetEditorWorldContext(bool bEnsureIsGWorld = false);
     {
+        /** kwakkh
+         * - 다양한 월드 타입에서 Editor 타입에 해당하는 월드를 가져오는 로직
+         * - 중요한 점은 만일 Editor 월드가 존재하지 않으면 check(false) 가 되어 무조건 크래시가 발동하게 되어 있다.
+         * - 언리얼 Editor 월드가 존재하지 않으면 Editor에서 어떤 것도 진행 불가이기 때문에 1개는 반드시 보장되어야 한다.
+         */
+
         for (int32 i=0; i < WorldList.Num(); ++i)
         {
             if (WorldList[i].WorldType == EWorldType::Editor)
@@ -204,3 +230,57 @@ public:
         FWorldContext& EditorContext = GetEditorWorldContext();
     }
 };
+
+/**
+ * TIndirectArray 와 TArray 의 차이
+ * - 가장 큰 차이는 **배열이 요소를 소유하고 관리하는 방식**
+ * 
+ * TArray : **실제 물건(값)**을 상자에 순서대로 넣어 보관하는 **창고**
+ * TIndirectArray : 물건은 다른 곳(힙 메모리)에 두고, 그 물건의 **위치가 적힌 쪽지(포인터)**만 순서대로 보관하는 **장부**
+ * 
+ * ----------------------------
+ * TArray<T> 값 배열
+ * 
+ * 우리가 흔히 아는 표준 배열
+ * TArray<FMyStruct> MyArray
+ * 
+ * - 동작 방식: FMyStruct 타입의 인스턴스(값) 자체가 배열 메모리 안에 순서대로 저장
+ * - 장점
+ *      1. 데이터가 한곳에 모여있어 캐시 효율이 좋고 접근이 매우 빠르다.
+ * - 단점
+ *      1. 다형성(Polymorphism) 불가: TArray<FBaseStruct>에 자식 클래스인 FDerivedStruct를 넣으면, 자식 클래스의 데이터가 잘려나가는 "Slicing" 현상이 발생
+ *      2. 느린 재배열: 만약 FMyStruct가 매우 큰 구조체라면, 배열 중간에 요소를 추가/삭제할 때마다 이 무거운 데이터를 복사하고 이동시키는 비용이 비싸다.
+ * 
+ * ----------------------------
+ * TIndirectArray<T> 간접 배열
+ * 
+ * 이름(Indirect)처럼 데이터를 '간접적으로' 가리킨다.
+ * TIndirectArray<FMyStruct> MyIndirectArray;
+ * 
+ * - 동작 방식
+ *      1. 요소(FMyStruct)를 추가(Add)하면, TIndirectArray가 힙(Heap) 메모리에 해당 요소를 위한 공간을 new로 할당
+ *      2. 배열 자체에는 그 힙 메모리를 가리키는 포인터만 저장
+ *      3. 배열에서 요소를 제거하거나 배열 자체가 파괴될 때, TIndirectArray는 자기가 가리키고 있던 힙 메모리를 자동으로 delete 한다. (중요!).
+ * - 장점
+ *      1. 다형성(Polymorphism) 완벽 지원: TIndirectArray<FBaseStruct>에 FDerivedStruct를 추가할 수 있다. 실제 저장은 포인터(FBaseStruct*)로 이루어지므로 데이터가 잘리지 않는다. 이것이 이 컨테이너의 핵심 존재 이유
+ *      2. 빠른 재배열: 배열이 재정렬될 때 실제 무거운 데이터가 아닌 가벼운 포인터만 교환됨.
+ * - 단점
+ *      1. TArray보다 접근 속도가 약간 느리다. (포인터를 한 번 더 거쳐가야 하므로)
+ * 
+ * ----------------------------
+ * TArray<T*>(포인터 배열)과 TIndirectArray<T> 의 차이가 뭔가?
+ * - TArray<FMyStruct*>: 포인터를 저장만 할 뿐, 그 포인터가 가리키는 객체의 생명 주기를 책임지지 않는다. new와 delete를 사용자가 직접 관리해야 하며, 안 그러면 메모리 누수(Memory Leak)가 발생
+ * - TIndirectArray<FMyStruct>: 포인터를 저장하며, 그 포인터가 가리키는 객체의 생명 주기(소유권)를 완벽하게 관리한다. Add 할 때 new 해주고, Remove나 소멸자가 호출될 때 delete 해준다.
+ * 
+ * ----------------------------
+ * 언제 사용해야 할까?
+ * - TArray<T>를 사용할 때 (95%의 경우):
+ *      1. int, FVector, FName, FGameplayTag 등 가벼운 데이터.
+ *      2. TObjectPtr<UObject> (어차피 UObject는 포인터로 관리됨).
+ *      3. 다형성이 필요 없는 struct를 다룰 때.
+ *      4. 고민된다면 일단 TArray를 써라.
+ * 
+ * TIndirectArray<T>를 사용할 때 (특수한 경우):
+ *      1. UObject가 아닌 일반 C++ struct나 class에서 다형성을 구현해야 할 때. (예: TIndirectArray<FBaseShape>에 FSquare, FCircle 등을 담을 때)
+ *      2. 배열이 해당 객체의 **소유권(new/delete)**을 명확하게 갖게 하고 싶을 때.
+ */
